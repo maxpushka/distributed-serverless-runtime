7b363537712de2dc2b91af99ccb4c4b6fb3819b3
Max Pushkarov <maxpushka200@gmail.com>
2024-12-02 16:32:50 +0200

    feat: add basic serverless executor & support for JS (#2)

--- executor/executor.go
+++ executor/executor.go
@@ -1,1 +1,36 @@
 package executor
+
+import (
+	"sync"
+)
+
+type Executor interface {
+	Execute(id string, cfg map[string]string, req Request) (Response, error)
+}
+
+type Request struct {
+	Method  string            `json:"method"`
+	URL     string            `json:"url"`
+	Headers map[string]string `json:"headers"`
+	Body    any               `json:"body,omitempty"`
+}
+
+type Response struct {
+	StatusCode int               `json:"status_code,omitempty"`
+	Headers    map[string]string `json:"headers,omitempty"`
+	Body       string            `json:"body,omitempty"`
+}
+
+type PythonExecutor struct {
+	// https://github.com/kluctl/go-embed-python
+}
+
+type GoExecutor struct {
+	// https://github.com/traefik/yaegi
+}
+
+type Runner[Runtime any] struct {
+	Mu       sync.RWMutex
+	Checksum string
+	Runtime  Runtime
+}
