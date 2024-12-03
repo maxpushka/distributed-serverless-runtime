9ab97bc6a9830214436b7aee91e0133c7c69b99f
Max Pushkarov <maxpushka200@gmail.com>
2024-12-03 14:30:43 +0200

    docs(cdn): clarify the purpose of CDN interface methods

--- cdn/cdn.go
+++ cdn/cdn.go
@@ -2,16 +2,29 @@ package cdn
 
 import "io"
 
+// CDN defines the complete CDN interface
+// with Command and Query capabilities.
 type CDN interface {
 	Command
 	Query
 }
 
+// Command interface provides methods
+// for uploading content to the CDN.
 type Command interface {
-	Upload(id string, content io.Reader) error
+	// Upload uploads a single file with the given ID and content.
+	Upload(id string, files []io.Reader) error
 }
 
+// Query interface provides methods
+// for reading and checking files in the CDN.
 type Query interface {
-	ReadFile(id string) (content io.Reader, checksum string, err error)
+	// ReadFiles retrieves multiple files associated with a given ID.
+	ReadFiles(id string) (files []io.Reader, checksum string, err error)
+
+	// Checksum retrieves a single checksum
+	// for all files associated with the given ID.
+	// It computes individual checksums,
+	// sorts them, concatenates them, and hashes the result.
 	Checksum(id string) (string, error)
 }
